de830e0f37f24e47c9dd60eb1ed10cb64e3076d8
kitodohellover-tech <kitodo.hellover@gmail.com>
2026-09-19 00:12:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ GROQ_KEY = os.getenv("GROQ_API_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # ⚠️ ЗАМЕНИ НА СВОЙ ID И ID ДРУЗЕЙ
-ALLOWED_IDS = [8981457970]
+ALLOWED_IDS = [8834374199]
 
 client = AsyncOpenAI(
     base_url="https://api.groq.com/openai/v1",
